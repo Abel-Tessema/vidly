85c0cab1d83c942ae0128fe1b81d7b3550a739ab
Bela Jash <abelxotessema@gmail.com>
2024-08-27 13:49:36 +0300

    Integration test: Prepare the movie returns test.

---
+++ tests/integration/routes/returns.test.js
@@ -0,0 +1,48 @@
+/*
+ * POST /api/returns {customerId, movieId}
+ *
+ * return a 401 status if the client is not logged in
+ * return a 400 status if customerId is not provided
+ * return a 400 status if movieId is not provided
+ * return a 404 status if there\'s no rental with the given customerId and movieId
+ * return a 400 status if the rental is already processed
+ * return a 200 status if it\'s a valid request
+ * set the return date
+ * calculate the rental fee
+ * increase the movie\'s stock
+ * return the rental object
+ */
+
+let server;
+require('../../../startup/validation')();
+const {Rental} = require('../../../models/rental');
+const mongoose = require("mongoose");
+
+describe('/api/returns', () => {
+  let customerId;
+  let movieId;
+  let rental;
+  
+  beforeEach(async () => {
+    server = require('../../../index');
+    customerId = new mongoose.Types.ObjectId().toHexString();
+    movieId = new mongoose.Types.ObjectId().toHexString();
+    
+    rental = new Rental({
+      customer: {_id: customerId, name: 'Name 1', phone: '12345'},
+      movie: {_id: movieId, title: 'Movie 1', dailyRentalRate: 3}
+    });
+    
+    await rental.save();
+  });
+  
+  afterEach(async () => {
+    server.close();
+    await Rental.deleteMany();
+  });
+  
+  it('should work', async () => {
+    const result = await Rental.findById(rental._id);
+    expect(result).toBeDefined();
+  });
+});
